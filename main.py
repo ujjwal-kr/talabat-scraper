eb868dcc141c509433d0a4b8c11a3f92634c5f70
Ujjwal Kumar <epicujjwal@protonmail.com>
2022-06-17 22:54:37 +0530

    format dict

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ sample_data = [
     "https://www.talabat.com/uae/restaurant/621796/pizza-di-rocco-jumeirah-lakes-towers--jlt?aid=1308"
 ]
 
+
 class Scraper:
     def __init__(self, sample_data):
         self.urls = sample_data
         self.restaurant_data = []
+
     def scrape(self):
         print("Please Wait...")
         for url in self.urls:
             resp = requests.get(url)
             # handle 404
             soup = BeautifulSoup(resp.content, 'html.parser')
-            json_dict = json.loads(soup.find('script', {'id': '__NEXT_DATA__'}).contents[0])['props']['pageProps']['initialMenuState']
+            json_dict = json.loads(soup.find('script', {'id': '__NEXT_DATA__'}).contents[0])[
+                'props']['pageProps']['initialMenuState']
             restaurant_name = json_dict['restaurant']['name']
             restaurant_logo = json_dict['restaurant']['logo']
             latitude = json_dict['restaurant']['latitude']
@@ -32,9 +35,12 @@ class Scraper:
                 menu_items['item_description'] = item['description']
                 menu_items['item_price'] = item['price']
                 menu_items['items_image'] = item['image']
-            self.restaurant_data.append([restaurant_name, restaurant_logo, latitude, longitude, cuisine_tags, menu_items])
-        self.pretty_print()    
+            self.restaurant_data.append([{'restaurant_name': restaurant_name}, {'restaurant_logo': restaurant_logo}, {
+                                        'latitude': latitude}, {'longitude': longitude}, {'cuisine_tags': cuisine_tags}, {'menu_items': menu_items}])
+        self.pretty_print()
+
     def pretty_print(self):
         print(self.restaurant_data)
+
 
 data = Scraper(sample_data).scrape()
