860de6c9f93344b9f295d1c82b554e33ada5389b
Ujjwal Kumar <epicujjwal@protonmail.com>
2022-06-18 09:26:22 +0530

    load more restaurants and pretty print pandas

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ sample_data = [
     "https://www.talabat.com/uae/restaurant/645430/pasta-della-nona-jlt-jumeirah-lakes-towers?aid=1308",
     "https://www.talabat.com/uae/restaurant/50445/pizzaro-marina-3?aid=1308",
     "https://www.talabat.com/uae/restaurant/605052/the-pasta-guyz-dubai-marina?aid=1308",
-    "https://www.talabat.com/uae/restaurant/621796/pizza-di-rocco-jumeirah-lakes-towers--jlt?aid=1308"
+    "https://www.talabat.com/uae/restaurant/621796/pizza-di-rocco-jumeirah-lakes-towers--jlt?aid=1308",
+    "https://www.talabat.com/uae/restaurant/628551/shawarmatacjumeirah-1?aid=1280",
+    "https://www.talabat.com/uae/restaurant/26820/mcdonalds-jumeirah1?aid=1302",
+    "https://www.talabat.com/uae/restaurant/664597/tostatto-cafe?aid=1486",
+    "https://www.talabat.com/uae/restaurant/32444/wingstop-mirdif?aid=1198",
+    "https://www.talabat.com/uae/restaurant/606319/chickzoneal-raffa?aid=1187"
 ]
 
 
@@ -40,12 +45,18 @@ class Scraper:
                 menu_item['item_price'] = item['price']
                 menu_item['items_image'] = item['image']
                 menu_items.append(menu_item)
-            self.restaurant_data.append([{'restaurant_name': restaurant_name}, {'restaurant_logo': restaurant_logo}, {
-                                        'latitude': latitude}, {'longitude': longitude}, {'cuisine_tags': cuisine_tags}, {'menu_items': menu_items}])
+            self.restaurant_data.append({'restaurant_name': restaurant_name, 'restaurant_logo': restaurant_logo, 
+                                        'latitude': latitude, 'longitude': longitude, 'cuisine_tags': cuisine_tags, 'menu_items': menu_items})
         self.pretty_print()
+        self.export_to_csv()
 
     def pretty_print(self):
-        print(self.restaurant_data)
+        df = pd.DataFrame(self.restaurant_data)
+        print(df)
+
+    def export_to_csv(self):
+        df = pd.DataFrame(self.restaurant_data)
+        df.to_csv("data.csv", sep="\t", encoding="utf-8")
 
 
 data = Scraper(sample_data).scrape()
